f1dfa51346f52535ba2bed5534294476e6fc4722
tyrannosaurusdm92 <tyrannosaurus.dm92@gmail.com>
2026-06-15 08:07:35 -0400

    Add files via upload

---
+++ games/developer_v1.js
@@ -0,0 +1,3 @@
+/* OurSpace local stub for missing developer_v1.js.
+   Preserves game loading by satisfying the optional external developer/analytics script path. */
+(function(){ window.__ourspaceDeveloperStubLoaded = true; })();
