dfdf588f62f35ed01c73074ca8924237be3a8796
tyrannosaurusdm92 <tyrannosaurus.dm92@gmail.com>
2026-06-22 07:05:11 -0400

    Delete games directory

--- games/developer_v1.js
+++
@@ -1,3 +0,0 @@
-/* OurSpace local stub for missing developer_v1.js.
-   Preserves game loading by satisfying the optional external developer/analytics script path. */
-(function(){ window.__ourspaceDeveloperStubLoaded = true; })();
